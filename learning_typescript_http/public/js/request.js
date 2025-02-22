170c4073c9d45384e32994477a072bdc7e5f86cb
DairanDoo <your_email@example.com>
2025-02-22 14:38:58 -0400

    Cuerpo de metodos get, put, post y delete finaizados en request.ts

--- learning_typescript_http/public/js/request.js
+++ learning_typescript_http/public/js/request.js
@@ -1,4 +1,36 @@
 "use strict";
-(() => {
-    const name = "nombre";
-});
+class Petition {
+    constructor(baseUrl) {
+        this.baseUrl = "";
+        this.headers = {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        };
+        this.baseUrl = baseUrl;
+    }
+    request(url, method = "POST", data = null) {
+        url = `${this.baseUrl}${url}`;
+        const options = { headers: this.headers, method };
+        if (data)
+            options.body = JSON.stringify(Object.assign({}, data));
+        return window.fetch(url, options);
+    }
+    get(path) {
+        const method = "GET";
+        return this.request(path, method).then((res) => res.json());
+    }
+    post(path, data) {
+        const method = "POST";
+        return this.request(path, method, data).then((res) => res.json());
+    }
+    update(path, data) {
+        const method = "PUT";
+        return this.request(path, method, data).then((res) => res.json());
+    }
+    delete(path, id) {
+        const method = "DELETE";
+        if (id)
+            path = `${path}/${id}`;
+        return this.request(path, method).then((res) => res.json());
+    }
+}
--- learning_typescript_http/src/request.ts
+++ learning_typescript_http/src/request.ts
@@ -13,9 +13,9 @@ interface IPetition {
     baseUrl: string
     headers: Header
     request<T>(url: string, method?: string, data?: T | null): Promise<Response>
-    get<T>(path: string, id: string): Promise<T>
+    get<T>(path: string): Promise<T>
     post<T>(path: string, data: T): Promise<T>
-    update<T>(path: string, id: string, data: T): Promise<T>
+    update<T>(path: string, data: T): Promise<T>
     delete<T>(path: string, id: string): Promise<T>
 }
 
@@ -40,17 +40,22 @@ class Petition implements IPetition {
         return window.fetch(url, options)
     }
 
-    get<T>(path: string, id: string): Promise<T> {
-        throw new Error("Method not implemented.")
+    get<T>(path: string): Promise<T> {
+        const method = "GET"
+        return this.request(path, method).then((res) => res.json())
     }
     post<T>(path: string, data: T): Promise<T> {
-        throw new Error("Method not implemented.")
+        const method = "POST"
+        return this.request(path, method, data).then((res) => res.json())
     }
-    update<T>(path: string, id: string, data: T): Promise<T> {
-        throw new Error("Method not implemented.")
+    update<T>(path: string, data: T): Promise<T> {
+        const method = "PUT"
+        return this.request(path, method, data).then((res) => res.json())
     }
     delete<T>(path: string, id: string): Promise<T> {
-        throw new Error("Method not implemented.")
+        const method = "DELETE"
+        if (id) path = `${path}/${id}` // si existe un id se concatena al final de la url
+        return this.request(path, method).then((res) => res.json())
     }
 }
 
